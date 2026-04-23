d9de8315cc28da052ae1fef67036446f23d9b23d
Zaskia Alya Fadilah <zaskialyafadilah@upi.edu>
2026-04-24 00:20:19 +0700

    perbaikan minor

--- resources/views/session-seller/seller-produk.blade.php
+++ resources/views/session-seller/seller-produk.blade.php
@@ -6,7 +6,7 @@
             <!-- Content -->
 
             <div class="container-xxl flex-grow-1 container-p-y">
-              <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Tabel /</span> Tabel Produk</h4>>
+              <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Tabel /</span> Tabel Produk</h4>
               <!-- Tabel Produk -->
               <div class="card">
                 <div class="card-header d-flex justify-content-between align-items-center">
