3d5f722cc75c4842cd8dc1fd4179e856c30f8585
Nandini Ananda Syifa <nandiniananda@upi.edu>
2026-04-30 07:47:48 +0700

    Menyelesaikan fitur dashboard seller dan tabel riwayat pesanan KantinQ

--- resources/views/session-seller/layout-seller.blade.php
+++ resources/views/session-seller/layout-seller.blade.php
@@ -138,8 +138,8 @@
 
           <ul class="menu-inner py-1">
             <!-- Dashboard -->
-            <li class="menu-item active">
-              <a href="index.html" class="menu-link">
+            <li class="menu-item  {{ Request::routeIs('master') ? 'active' : '' }}"">
+              <a href="{{ route('master') }}" class="menu-link">
                 <i class="menu-icon tf-icons bx bx-home-circle"></i>
                 <div data-i18n="Analytics">Dashboard</div>
               </a>
